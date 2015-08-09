bff6488c5eb6b93c3340a8d4d9d804ecdfb7cf13
Antonio @ jemchicomac <jemchicomac@gmail.com>
2015-08-09 11:58:21 +0200

    initial commit

---
+++ desktop/src/com/jemchicomac/backfire/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jemchicomac.backfire.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jemchicomac.backfire.Backfire;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Backfire(), config);
+	}
+}
