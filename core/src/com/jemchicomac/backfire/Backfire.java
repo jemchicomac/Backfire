faff8f247ea19dfce5340edaecc6b095ec8472b1
Antonio @ jemchicomac <jemchicomac@gmail.com>
2015-08-11 15:14:13 +0200

    uiskin adapted

--- core/src/com/jemchicomac/backfire/Backfire.java
+++ core/src/com/jemchicomac/backfire/Backfire.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
@@ -101,7 +102,10 @@ public class Backfire extends ApplicationAdapter {
 	Array<String> names;
 	Stage stage;
 	Label label;
-
+	
+	BitmapFont retroFont;
+	
+	
 	public void create () {
 		stage = new Stage();
 		Skin skin = new Skin(Gdx.files.internal("data/uiskin.json"));
